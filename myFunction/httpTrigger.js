655d9f24d13d7272d18950d03dc9adb0da118fe3
GeraldOpitz <geraldopitz@gmail.com>
2023-11-29 18:18:14 -0300

    change

---
+++ myFunction/httpTrigger.js
@@ -0,0 +1,15 @@
+// Importar el módulo de logging
+const { logger } = require('azure-functions-core-tools');
+
+module.exports = async function (context, req) {
+    // Acceder a la variable de entorno
+    const myCustomVariable = process.env.MyCustomVariable;
+
+    // Imprimir el valor en los logs
+    logger.info(`Valor de MyCustomVariable: ${myCustomVariable}`);
+
+    // Responder a la solicitud HTTP
+    context.res = {
+        body: "¡Función ejecutada con éxito!"
+    };
+};
