ec7a80d9a10026a708d8a9d8242fba7e917f29f3
GeraldOpitz <geraldopitz@gmail.com>
2023-11-30 13:24:33 -0300

    testagain

--- myFunction/httpTrigger.js
+++ myFunction/httpTrigger.js
@@ -6,7 +6,7 @@ module.exports = async function (context, req) {
     const myCustomVariable = process.env.MyCustomVariable;
 
     // Imprimir el valor en los logs
-    logger.info(`Valor de MyCusal: ${myCustomVariable}`);
+    logger.info(`Valor de MyCual: ${myCustomVariable}`);
 
     // Responder a la solicitud HTTP
     context.res = {
